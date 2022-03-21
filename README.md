95258483a4d0f4145d3d08f28065476081d390d8
Kitorosano <rosanoesteban2@gmail.com>
2022-03-21 01:33:24 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SOFKA_TallerTDD-Avanzado
---
+++ src/main/java/uy/com/sofka/tddavanzado/TddavanzadoApplication.java
@@ -0,0 +1,13 @@
+package uy.com.sofka.tddavanzado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddavanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddavanzadoApplication.class, args);
+	}
+
+}
